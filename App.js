e7d99282782ba0583e7909a4260ab09107689197
Arthur Gramm <arthurgramm@outlook.com.br>
2020-04-03 16:30:08 -0300

    pieChart

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Text } from 'react-native-svg'
+import { PieChart } from 'react-native-svg-charts';
+
+export default function App() {
+
+  const data = [10, 20, 30, 40, 50];
+  const randomColor = () => ('#' + ((Math.random() * 0xffffff) << 0).toString(16) + '000000').slice(0, 7)
+  const pieData = data.map((value, index) => ({
+    value,
+    key: `${index}-${value}`,
+    svg: {
+      fill: randomColor()
+    }
+  }))
+
+  const Label = ({ slices }) => {
+    return slices.map((slice, index) => {
+      const {pieCentroid, data} = slice;
+      return (
+        <Text
+          key={`label-${index}`}
+          x={pieCentroid[0]}
+          y={pieCentroid[1]}
+          fill="black"
+          textAnchor={'middle'}
+          alignmentBaseline={'middle'}
+          fontSize={22}
+        >
+          {data.value}
+        </Text>
+      )
+    })
+  }
+
+  return (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <PieChart style={{height: 400}} data={pieData}>
+        <Label />
+      </PieChart>
+    </View>
+  );
+}
